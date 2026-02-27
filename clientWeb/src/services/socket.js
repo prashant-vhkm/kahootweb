5a86086e8277892035a77a73d6ebba8632288038
prashant-vhkm <prashantvhkm@gmail.com>
2026-02-27 17:46:18 +0530

    Update socket.js

--- clientWeb/src/services/socket.js
+++ clientWeb/src/services/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000", {
+export const socket = io("https://kahoot-server-w9v0.onrender.com", {
   transports: ["websocket"],
 });
